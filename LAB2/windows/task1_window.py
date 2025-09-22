8b021cb3fa9a3d81e8d0f4ef4a77e59d46db560b
Александр Черевко <acherevko@test.ru>
2025-09-23 00:09:06 +0300

    Update task1_window.py

--- LAB2/windows/task1_window.py
+++ LAB2/windows/task1_window.py
@@ -35,41 +35,37 @@ class Task1Window:
         output_folder = os.path.join(self.parent.output_path, "task1")
         os.makedirs(output_folder, exist_ok=True)
 
-        gray_image_average = self.rgb2gray_average(image_array)
-        gray_image_weighted = self.rgb2gray_weighted(image_array)
+        gray_image_ntsc = self.rgb2gray_ntsc(image_array)
+        gray_image_hdtv = self.rgb2gray_hdtv(image_array)
 
-        Image.fromarray(gray_image_average).save(
-            os.path.join(output_folder, "grayscale_average.jpg")
+        Image.fromarray(gray_image_ntsc).save(
+            os.path.join(output_folder, "grayscale_ntsc.jpg")
         )
-        Image.fromarray(gray_image_weighted).save(
-            os.path.join(output_folder, "grayscale_weighted.jpg")
+        Image.fromarray(gray_image_hdtv).save(
+            os.path.join(output_folder, "grayscale_hdtv.jpg")
         )
 
-        difference_image = np.abs(gray_image_average - gray_image_weighted)
+        difference_image = np.abs(gray_image_ntsc.astype(np.int16) - gray_image_hdtv.astype(np.int16)).astype(np.uint8)
         Image.fromarray(difference_image).save(
             os.path.join(output_folder, "grayscale_difference.jpg")
         )
 
-        self.plot_histograms(gray_image_average, gray_image_weighted)
+        self.plot_histograms(gray_image_ntsc, gray_image_hdtv)
 
-    def rgb2gray_average(self, img):
-        return (0.3 * img[:, :, 0] + 0.59 * img[:, :, 1] + 0.11 * img[:, :, 2]).astype(
-            np.uint8
-        )
+    def rgb2gray_ntsc(self, img):
+        return (0.299 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]).astype(np.uint8)
 
-    def rgb2gray_weighted(self, img):
-        return (0.21 * img[:, :, 0] + 0.72 * img[:, :, 1] + 0.07 * img[:, :, 2]).astype(
-            np.uint8
-        )
+    def rgb2gray_hdtv(self, img):
+        return (0.2126 * img[:, :, 0] + 0.7152 * img[:, :, 1] + 0.0722 * img[:, :, 2]).astype(np.uint8)
 
-    def plot_histograms(self, gray_image_average, gray_image_weighted):
+    def plot_histograms(self, gray_image_ntsc, gray_image_hdtv):
         fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 
-        axs[0].hist(gray_image_average.ravel(), bins=256, color="gray", alpha=0.75)
-        axs[0].set_title("Grayscale Image Histogram (Average Method)")
+        axs[0].hist(gray_image_ntsc.ravel(), bins=256, color="gray", alpha=0.75)
+        axs[0].set_title("Grayscale Histogram (NTSC/PAL Standard)")
 
-        axs[1].hist(gray_image_weighted.ravel(), bins=256, color="gray", alpha=0.75)
-        axs[1].set_title("Grayscale Image Histogram (Weighted Method)")
+        axs[1].hist(gray_image_hdtv.ravel(), bins=256, color="gray", alpha=0.75)
+        axs[1].set_title("Grayscale Histogram (HDTV Standard)")
 
+        plt.tight_layout()
         plt.show()
-        return
